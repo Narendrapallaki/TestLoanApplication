a2c0c36a70873cb895490b30108b793698e98ad3
Narendra reddy <narendrapallaki@gmail.com>
2024-05-20 23:37:35 -0700

    bill payment

---
+++ BillPayment/src/main/java/com/billPayment/entity/Order.java
@@ -0,0 +1,17 @@
+package com.billPayment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+	
+	
+	public String type;
+	public int discount;
+	
+
+}
